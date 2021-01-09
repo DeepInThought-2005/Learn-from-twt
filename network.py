9ff4212ea1d8d6afe9de4cee27f706e8ec24a7a0
Louis <68382039+JDT-MYDCC@users.noreply.github.com>
2021-01-09 21:43:56 +0100

    Add files via upload

---
+++ network.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time      : 9/1/21 4:58 pm
+# @Author    : Louis
+# @File      : network
+# @Software  : PyCharm
+# Description:
+
+
+import socket
+import pickle
+
+
+class Network:
+    def __init__(self):
+        self.client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.server = "192.168.0.32"
+        self.port = 5555
+        self.addr = (self.server, self.port)
+        self.p = self.connect()
+
+    def getP(self):
+        return self.p
+
+    def connect(self):
+        try:
+            self.client.connect(self.addr)
+            return pickle.loads(self.client.recv(2048))
+        except:
+            pass
+
+    def send(self, data):
+        try:
+            self.client.send(pickle.dumps(data))
+            return pickle.loads(self.client.recv(2048))
+        except socket.error as e:
+            print(e)
+
+
